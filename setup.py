4c49361a4659e07d02b584023f7313dd19d24989
koppen021 <p.koppen@amsterdam.nl>
2022-06-13 22:51:40 +0200

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='afval',
+    version='0.0.1',
+    packages=['demo', 'afval', 'afval.osm', 'afval.graph'],
+    url='https://github.com/wpk-/afval/',
+    license='MIT',
+    author='Paul Koppen',
+    author_email='p.koppen@amsterdam.nl',
+    description='Python library for Gemeente Amsterdam, department of Afval & Grondstoffen'
+)
